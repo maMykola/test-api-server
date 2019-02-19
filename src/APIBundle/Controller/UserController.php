d3621c3083f9a375dfabd675f1cde9e8cb9f8d7f
Mykola Martynov <mykola.martynov@hotmail.com>
2019-02-19 11:38:58 +0200

    [API] User: user info by id

--- src/APIBundle/Controller/UserController.php
+++ src/APIBundle/Controller/UserController.php
@@ -46,7 +46,7 @@ class UserController extends Controller
      */
     public function infoAction(User $user)
     {
-        return new JsonResponse([]);
+        return new JsonResponse($this->user_info($user));
     }
 
     /**
